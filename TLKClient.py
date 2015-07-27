11c311a4f912692b56f6e0fb168e0c5fd96f5ed0
Richard Weber <richard.weber@ksfmedia.fi>
2015-07-27 15:27:25 +0300

    Project init

---
+++ TLKClient.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import yaml, os
+
+app = Flask(__name__)
+app.debug = True
+config = None
+
+@app.route('/')
+def hello_world():
+    the_url = config['backend']['url']
+    return render_template("index.html", the_url=the_url)
+
+
+if __name__ == '__main__':
+    #Read configuration
+    with open("%s/config.yml" % os.path.dirname(os.path.realpath(__file__)), 'r') as f:
+        config = yaml.load(f)
+    app.run()
